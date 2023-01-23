03c5071ad369da20bd4b725c339315febdd8f9f6
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-01-23 11:09:46 +0100

    onboarding screen implementation completed

--- lib/onboarding/screens/onboarding_screen.dart
+++ lib/onboarding/screens/onboarding_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/onboarding/utilities/onboarding_strings.dart';
+import 'package:task_manager/utilities/app_constants/app_colors.dart';
+import 'package:task_manager/utilities/app_constants/app_strings.dart';
 import 'package:task_manager/utilities/device_size.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -10,7 +13,58 @@ class OnboardingScreen extends StatelessWidget {
     final deviceWidth = context.screenWidth();
     return Scaffold(
       body: Column(
-        children: [Image.asset("asset/images/onboarding.png")],
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+                top: deviceHeight / 15, bottom: deviceHeight / 30),
+            alignment: Alignment.center,
+            child: Image.asset(
+              OnboardingStrings.onboardingAsset,
+              alignment: Alignment.center,
+            ),
+          ),
+          const Text(
+            OnboardingStrings.appName,
+            style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 32,
+                fontFamily: AppStrings.fontName),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 16, right: 16),
+            child: Text(
+              OnboardingStrings.appHeader,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: AppStrings.fontName,
+                  fontWeight: FontWeight.w400),
+            ),
+          ),
+          const Spacer(),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            width: double.infinity,
+            height: context.screenHeight() / 14,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: AppColors.green, borderRadius: BorderRadius.circular(4)),
+            child: const Text(OnboardingStrings.continueW,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                fontFamily: AppStrings.fontName,
+                color: AppColors.white
+              ),
+            ),
+          ),
+          const Spacer(),
+        ],
       ),
     );
   }
---
+++ lib/onboarding/utilities/onboarding_strings.dart
@@ -0,0 +1,6 @@
+class OnboardingStrings{
+  static const onboardingAsset =  "asset/images/onboarding.png";
+  static const appName = "Task Manager";
+  static const appHeader = "This smart tool is designed to help you better manage your tasks!";
+  static const continueW = "Continue";
+}
