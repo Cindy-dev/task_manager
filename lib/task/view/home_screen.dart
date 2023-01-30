ba656b1654803370c02d7f8c9ca18149528bd0e7
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-01-30 11:29:04 +0100

    home_screen design implementation

--- lib/task/view/home_screen.dart
+++ lib/task/view/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:task_manager/task/utilities/task_strings/home_strings.dart';
 import 'package:task_manager/utilities/app_constants/app_colors.dart';
-
 import '../../utilities/app_constants/app_strings.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -32,12 +31,43 @@ class HomeScreen extends StatelessWidget {
             child: const Text(
               HomeStrings.priority,
               style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  fontFamily: AppStrings.fontName,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                fontFamily: AppStrings.fontName,
               ),
             ),
           ),
+          Container(
+              margin: const EdgeInsets.all(16),
+              padding: const EdgeInsets.all(16),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: AppColors.gray.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Text Uchechukwu",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: AppStrings.fontName,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Every 5 minutes",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: AppStrings.fontName,
+                    ),
+                  ),
+                ],
+              )),
         ],
       ),
     );
