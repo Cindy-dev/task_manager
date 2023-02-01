f1105eae1862ff0109e16e1b2fd752f24dd7cbac
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-02-01 11:45:33 +0100

    date pickers

--- lib/task/widgets/add_new_task.dart
+++ lib/task/widgets/add_new_task.dart
@@ -1,4 +1,6 @@
+import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:task_manager/task/utilities/task_strings/add_task_strings.dart';
 import 'package:task_manager/utilities/app_constants/app_colors.dart';
 import 'package:task_manager/utilities/device_size.dart';
@@ -19,56 +21,69 @@ Future<dynamic> addNewTask(BuildContext context) {
         return Container(
           height: MediaQuery.of(context).size.height,
           child: SingleChildScrollView(
-            child: Container(
-              margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.horizontal_rule,
-                        size: 100,
-                        color: AppColors.gray,
-                      )),
-                  const Text(
-                    AddTaskStrings.heading,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      fontFamily: AppStrings.fontName,
-                    ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                // SfCalendar(
+                //   todayHighlightColor: AppColors.primaryColor,
+                //   view: CalendarView.timelineMonth,
+                //
+                // ),
+                const Icon(
+                  Icons.horizontal_rule,
+                  size: 60,
+                  color: AppColors.gray,
+                ),
+                DateTimePicker(
+                  initialValue: '',
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2100),
+                  dateLabelText: 'Date',
+                  onChanged: (val) => print(val),
+                  validator: (val) {
+                    print(val);
+                    return null;
+                  },
+                  onSaved: (val) => print(val),
+                ),
+                const Text(
+                  AddTaskStrings.heading,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: AppStrings.fontName,
                   ),
-                  ListTile(
-                    leading: new Icon(Icons.photo),
-                    title: new Text('Photo'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: new Icon(Icons.music_note),
-                    title: new Text('Music'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: new Icon(Icons.videocam),
-                    title: new Text('Video'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: new Icon(Icons.share),
-                    title: new Text('Share'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
+                ),
+                ListTile(
+                  leading: new Icon(Icons.photo),
+                  title: new Text('Photo'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: new Icon(Icons.music_note),
+                  title: new Text('Music'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: new Icon(Icons.videocam),
+                  title: new Text('Video'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: new Icon(Icons.share),
+                  title: new Text('Share'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
             ),
           ),
         );
