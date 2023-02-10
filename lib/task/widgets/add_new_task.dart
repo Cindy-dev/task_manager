acde478d530b89fe5252d4c62c866d44f19c8082
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-02-10 12:31:58 +0100

    add task buttons implementation

--- lib/task/widgets/add_new_task.dart
+++ lib/task/widgets/add_new_task.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:task_manager/task/utilities/task_strings/add_task_strings.dart';
 import 'package:task_manager/task/widgets/tile_container.dart';
 import 'package:task_manager/utilities/app_constants/app_colors.dart';
+import 'package:task_manager/utilities/device_size.dart';
 import '../../utilities/app_constants/app_strings.dart';
 
 Future<dynamic> addNewTask(BuildContext context) {
@@ -96,105 +96,47 @@ Future<dynamic> addNewTask(BuildContext context) {
                 const SizedBox(
                   height: 30,
                 ),
-              ],
-            ),
-          ),
-        );
-      });
-}
-
-import 'package:syncfusion_flutter_calendar/calendar.dart';
-import 'package:task_manager/task/utilities/task_strings/add_task_strings.dart';
-import 'package:task_manager/task/widgets/tile_container.dart';
-import 'package:task_manager/utilities/app_constants/app_colors.dart';
-import '../../utilities/app_constants/app_strings.dart';
-
-Future<dynamic> addNewTask(BuildContext context) {
-  return showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(20),
-        ),
-      ),
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      builder: (context) {
-        return Container(
-          margin: const EdgeInsets.all(15),
-          height: MediaQuery.of(context).size.height,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Icon(
-                  Icons.horizontal_rule,
-                  size: 60,
-                  color: AppColors.gray,
-                ),
-                const Text(
-                  AddTaskStrings.heading,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                    fontFamily: AppStrings.fontName,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SfCalendar(
-                  todayHighlightColor: AppColors.primaryColor,
-                  view: CalendarView.timelineMonth,
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                TextFormField(
-                    minLines: 10,
-                    maxLines: 40,
-                    cursorColor: AppColors.primaryColor,
-                    decoration: InputDecoration(
-                        filled: true,
-                        hintText: AddTaskStrings.hintText,
-                        fillColor: AppColors.gray.withOpacity(0.3),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                                width: 2,
-                                color: AppColors.gray.withOpacity(0.3))),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                                width: 2,
-                                color: AppColors.gray.withOpacity(0.3))))),
-                const SizedBox(
-                  height: 30,
-                ),
-                const TileContainer(
-                  text: 'Task Time',
-                  iconHeader: Icons.alarm,
-                  arrowFwd: Icons.arrow_forward_ios,
-                  time: '4:00 PM',
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const TileContainer(
-                  text: 'Notification',
-                  iconHeader: Icons.notifications,
-                  arrowFwd: Icons.arrow_forward_ios,
-                  time: 'in 5 min',
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const TileContainer(
-                  text: 'Repeat',
-                  iconHeader: Icons.repeat,
-                  arrowFwd: Icons.arrow_forward_ios,
-                  time: 'No',
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      height: context.screenHeight() / 15,
+                      width: context.screenWidth() / 2.5,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                            color: AppColors.black,
+                          )),
+                      child: const Text(
+                        "Cancel",
+                        style: TextStyle(
+                            color: AppColors.black,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: AppStrings.fontName),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      height: context.screenHeight() / 15,
+                      width: context.screenWidth() / 2.5,
+                      decoration: BoxDecoration(
+                        color: AppColors.black,
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                            color: AppColors.black,
+                          )),
+                      child: const Text(
+                        "Save task",
+                        style: TextStyle(
+                            color: AppColors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: AppStrings.fontName),
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(
                   height: 30,
