d0c5826f86b613a4e9c4fd0e1daf118ebf8c8d24
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-03-03 19:20:46 +0100

    implementing slide transition on task tab

--- lib/task/models/add_task_model.dart
+++ lib/task/models/add_task_model.dart
@@ -1,17 +1,22 @@
-class AddTaskModel {
-  String id;
-  String taskDetails;
-  String taskDate;
-  String taskTime;
-  String taskNotification;
-  String taskRepeat;
+import 'package:equatable/equatable.dart';
 
-  AddTaskModel(
+class AddTaskModel extends Equatable {
+  final String id;
+  final String taskDetails;
+  final String taskDate;
+  final String taskTime;
+  final String taskNotification;
+  final String taskRepeat;
+
+  const AddTaskModel(
       {required this.id,
       required this.taskDetails,
       required this.taskTime,
       required this.taskDate,
       required this.taskNotification,
-      required this.taskRepeat
-      });
+      required this.taskRepeat});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
 }
--- lib/task/ui/view/home_screen.dart
+++ lib/task/ui/view/home_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_sheet/sliding_sheet.dart';
 import 'package:task_manager/task/utilities/task_strings/home_strings.dart';
 import 'package:task_manager/src/utilities/device_size.dart';
 import '../../../src/utilities/app_constants/app_colors.dart';
 import '../../../src/utilities/app_constants/app_strings.dart';
 import '../widgets/add_new_task.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +39,9 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: AppColors.white,
       body: Column(
         children: [
-          SizedBox(height: height * .065,),
+          SizedBox(
+            height: height * .065,
+          ),
           Container(
             margin: const EdgeInsets.all(16),
             alignment: Alignment.centerLeft,
@@ -28,37 +54,43 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-              margin: const EdgeInsets.all(16),
-              padding: const EdgeInsets.all(16),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: AppColors.gray.withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(15)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    "Text Uchechukwu",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      fontFamily: AppStrings.fontName,
+          SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(0, 1),
+              end: Offset.zero,
+            ).animate(_animationController),
+            child: Container(
+                margin: const EdgeInsets.all(16),
+                padding: const EdgeInsets.all(16),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: AppColors.gray.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "Text Uchechukwu",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: AppStrings.fontName,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Every 5 minutes",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                      fontFamily: AppStrings.fontName,
+                    SizedBox(
+                      height: 10,
                     ),
-                  ),
-                ],
-              )),
+                    Text(
+                      "Every 5 minutes",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        fontFamily: AppStrings.fontName,
+                      ),
+                    ),
+                  ],
+                )),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
