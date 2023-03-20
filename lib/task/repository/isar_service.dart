f25c5f69d3fb73ba78f5071c6c1e8962157ce25e
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2023-03-20 11:42:50 +0100

    adding required db query

--- lib/task/repository/isar_service.dart
+++ lib/task/repository/isar_service.dart
@@ -1,11 +1,40 @@
 import 'package:isar/isar.dart';
 
-class IsarService{
+import 'entities/add_task_db.dart';
+
+class IsarService {
   late Future<Isar> db;
 
   IsarService() {
     db = openIsar();
   }
 
-  
+  Future<Isar> openIsar() async {
+    if (Isar.instanceNames.isEmpty) {
+      //Passing my collection schema to a new Open Isar instance
+      return await Isar.open(
+        [AddTaskDBSchema],
+        inspector: true,
+      );
+    }
+
+    return Future.value(Isar.getInstance());
+  }
+
+  Future<void> createTask(AddTaskDB newTask) async {
+    final isar = await db;
+    // Insert or update tasks
+    isar.writeTxnSync<int>(() => isar.addTaskDBs.putSync(newTask));
+  }
+
+  Stream<List<AddTaskDB>> getAllTasks() async* {
+    final isar = await db;
+    yield* isar.addTaskDBs.where().watch(fireImmediately: true);
+  }
+
+
+  Future<void> cleanDb() async {
+    final isar = await db;
+    await isar.writeTxn(() => isar.clear());
+  }
 }
